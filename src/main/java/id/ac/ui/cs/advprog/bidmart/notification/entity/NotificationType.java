0091db82b515c538f489b6db245398d7bb62c8f4
Fidel <149506792+FidelAkilah@users.noreply.github.com>
2026-04-16 21:23:29 +0700

    feat(notification): add NotificationType and NotificationChannel enums

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/notification/entity/NotificationType.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.bidmart.notification.entity;
+
+public enum NotificationType {
+    AUCTION_WON,
+    AUCTION_LOST,
+    AUCTION_UNSOLD,
+    ORDER_CREATED,
+    ORDER_UPDATE,
+    NEW_BID,
+    TRANSACTION,
+    SYSTEM
+}
